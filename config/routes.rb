15a92c6e431f09ab4bb7f66f4d4e37823686c7de
jim.geer <jimgeer@comcast.net>
2021-03-30 10:40:24 -0700

    add enterprises

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :enterprises
   devise_for :users, controllers: { sessions: 'users/sessions' }
   root to: "homes#index"
   get 'homes/index'
-  skip_before_action :authenticate_user!, :only => [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_150530) do
+ActiveRecord::Schema.define(version: 2021_03_30_170759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "enterprises", force: :cascade do |t|
+    t.string "new_name"
+    t.string "new_acct"
+    t.string "parent"
+    t.boolean "active"
+    t.string "location_code"
+    t.string "location_name"
+    t.string "address_1"
+    t.string "address_2"
+    t.string "city"
+    t.string "state"
+    t.string "postal"
+    t.string "country"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_enterprises_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +46,5 @@ ActiveRecord::Schema.define(version: 2021_03_30_150530) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "enterprises", "users"
 end
