a211f761bf32f17fce06903a0e7fa700d5816283
jim.geer <jimgeer@comcast.net>
2021-06-28 09:32:17 -0700

    cleaned up presentation layer, added info to SO import page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password) }
   end
 
+  def authenticate_admin!
+    authenticate_user!
+    redirect_to users_path  unless current_user.admin?
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
 
-
-  before_action :set_user, only: %i[ show edit update destroy ]
-
   before_action :authenticate_user!
+  before_action :authenticate_admin!, only: [:destroy, :show, :edit] # `only` part if applicable
+  before_action :set_user, only: %i[ show edit update destroy ]
 
   def index
     @users = User.all
