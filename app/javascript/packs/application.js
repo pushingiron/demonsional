683bfc9c2a0429b5c88674032ef65be75e55011d
jim.geer <jimgeer@comcast.net>
2023-01-17 13:35:43 -0800

    fixed menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,14 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//= require jquery3
-//= require popper
-//= require bootstraps=sprockets
-
-import * as ActiveStorage from "@rails/activestorage"
-//= require bootstrap
-import "channels"
-//= require "trubolinks"
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("@popperjs/core");
+require("jquery");
+require("bootstrap");
+require("jquery-ui");
+var jQuery = require('jquery');
 
 
-Turbolinks.start()
-ActiveStorage.start()
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+alert("end");
