9b40afd3cd005bfb479f219871cb988b1238e725
jim.geer <jimgeer@comcast.net>
2023-01-23 15:02:21 -0800

    removed bootstrap from application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@ require("@rails/activestorage").start();
 require("channels");
 require("@popperjs/core");
 require("jquery");
-require("bootstrap");
 require("jquery-ui");
 var jQuery = require('jquery');
 
